5cde5bcc4c479b9b5caf7a326456468468095fb8
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2019-12-20 14:26:46 +0000

    Back to 3.7

--- install.sh
+++ install.sh
@@ -9,16 +9,16 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get -qq update
-apt-get -y install python3.8 python3.8-dev python3.8-venv
+apt-get -y install python3.7 python3.7-dev python3.7-venv
 apt-get -y install python3-distutils
 apt-get -y install gettext nodejs yarn git
 apt-get -y install poppler-utils
 apt-get -y autoremove
 
 rm /usr/bin/python3
-ln -s /usr/bin/python3.8 /usr/bin/python3
+ln -s /usr/bin/python3.7 /usr/bin/python3
 rm /usr/bin/python
-ln -s /usr/bin/python3.8 /usr/bin/python
+ln -s /usr/bin/python3.7 /usr/bin/python
 
 curl -s https://bootstrap.pypa.io/get-pip.py | python
 
